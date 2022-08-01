0a3304b1c4e60d977b9a7c29e729faf01db096fc
Can-Tkcu <can.tkcu@hotmail.com>
2022-08-01 12:16:05 +0200

    added navbar

---
+++ script.js
@@ -0,0 +1,43 @@
+let posts = [
+    {
+        'author': 'Author1',
+        'image': 'img/img1.jpg',
+        'description': 'Text123',
+        'location': 'Germany'
+    },
+    {
+        'author': 'Author2',
+        'image': 'img/img1.jpg',
+        'description': 'Text123',
+        'location': 'Germany'
+    },
+    {
+        'author': 'Author3',
+        'image': 'img/img1.jpg',
+        'description': 'Text123',
+        'location': 'Germany'
+    },
+    {
+        'author': 'Author4',
+        'image': 'img/img1.jpg',
+        'description': 'Text123',
+        'location': 'Germany'
+    },
+    {
+        'author': 'Author5',
+        'image': 'img/img1.jpg',
+        'description': 'Text123',
+        'location': 'Germany'
+    },
+    {
+        'author': 'Author6',
+        'image': 'img/img1.jpg',
+        'description': 'Text123',
+        'location': 'Germany'
+    },
+];
+
+
+function submitSearch() {
+    console.log('searched')
+}
