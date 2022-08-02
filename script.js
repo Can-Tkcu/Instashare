f1c5ff940347e3b8d4d9ed78a859d462c6e917d3
Can-Tkcu <can.tkcu@hotmail.com>
2022-08-02 17:04:45 +0200

    finished layout

--- script.js
+++ script.js
@@ -2,36 +2,42 @@ let posts = [
     {
         'author': 'Author1',
         'image': 'img/bootstrap-icons-1.9.1/person-circle.svg',
+        'content': 'img/image-01.jpg',
         'description': 'Text123',
         'location': 'Germany'
     },
     {
         'author': 'Author2',
         'image': 'img/bootstrap-icons-1.9.1/person-circle.svg',
+        'content': 'img/image-02.jpg',
         'description': 'Text123',
         'location': 'Germany'
     },
     {
         'author': 'Author3',
         'image': 'img/bootstrap-icons-1.9.1/person-circle.svg',
+        'content': 'img/image-03.jpg',
         'description': 'Text123',
         'location': 'Germany'
     },
     {
         'author': 'Author4',
         'image': 'img/bootstrap-icons-1.9.1/person-circle.svg',
+        'content': 'img/image-04.jpg',
         'description': 'Text123',
         'location': 'Germany'
     },
     {
         'author': 'Author5',
         'image': 'img/bootstrap-icons-1.9.1/person-circle.svg',
+        'content': 'img/image-05.jpg',
         'description': 'Text123',
         'location': 'Germany'
     },
     {
         'author': 'Author6',
         'image': 'img/bootstrap-icons-1.9.1/person-circle.svg',
+        'content': 'img/image-06.jpg',
         'description': 'Text123',
         'location': 'Germany'
     },
@@ -59,26 +65,30 @@ function render() {
     for (let i = 0; i < posts.length; i++) {
         const post = posts[i];
 
-        postContainer.innerHTML += `
+        postContainer.innerHTML += /*html*/`
         <div class="post-card">
-            <div class="post-card-wrapper">
-                <div>
-                     <img src="${post["image"]}">
-                </div>
-                <div>
+            <div class="card-header-wrapper">
+                <div class="card-header-container">
+                    <div class="authorPP-container">
+                        <img src="${post["image"]}">
+                    </div>
                     <div class="author-wrapper">
-                        ${post["author"]}
+                        <div>
+                            <span><b>${post["author"]}</b></span>
+                         </div>
+                         <span>${post["location"]}</span> -
+                         <span>${post["description"]}</span>
                     </div>
-                    <div>
-                        ${post["location"]}
-                    </div>
                 </div>
-                <div>
-                     ${post["description"]}
+                <div class="post-card-options">
+                    <img src="img/bootstrap-icons-1.9.1/three-dots.svg">
                 </div>
-                <div>
-                    options
-                </div>
+            </div>
+            <div class="card-content">
+                <img src="${post["content"]}">
+            </div>
+            <div class="card-bottom-section">
+
             </div>
         </div>`;
     }
