6d153651ae0ae5c76711b9ec8d152e1feb56ed75
Can-Tkcu <can.tkcu@hotmail.com>
2022-08-17 14:31:47 +0200

    save

--- script.js
+++ script.js
@@ -49,6 +49,8 @@ let posts = [
   },
 ];
 
+let emojiOpened = false;
+
 loadApp();
 
 let emojis = ["😍", "🥰", "😂", "😀", "❤", "💯"];
@@ -56,22 +58,27 @@ let emojis = ["😍", "🥰", "😂", "😀", "❤", "💯"];
 let postContainer = document.getElementById("postCards");
 
 /** TODO's
+ * style icons maybe ?
  */
 
 //   function submitSearch() {
 //   console.log("searched");
-// } 
+// }
+
+function hideIcon(self) {
+  self.style.backgroundImage = "none";
+}
 
 function filterNames() {
   let search = document.getElementById("name").value;
   search = search.toLowerCase();
-  console.log(search);
   postContainer.innerHTML = "";
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
     if (post["author"].toLowerCase().includes(search)) {
       postContainer.innerHTML = generatePostCard(i);
       renderEmojis(i);
+      render();
     }
   }
 }
@@ -90,9 +97,9 @@ function submitComment(index) {
 }
 
 function deleteComment(index, comment) {
-    posts[index]["comments"].splice(comment);
-    renderComments();
-    saveApp(); 
+  posts[index]["comments"].splice(comment, 1);
+  renderComments();
+  saveApp();
 }
 
 function liked(i, like) {
@@ -116,7 +123,12 @@ function toggleScrollRight() {
   const element = document.getElementById("scrollables");
   element.scrollLeft += 200;
 }
-////////////////////////////////////////////////////////////////////////////////////////////RENDER FUNCS////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function scrollToBottom(index) {
+  let element = document.getElementById(`commentOUT${index}`);
+  element.scrollTop += element.scrollHeight;
+}
+////////////////////////////////////////////////////////////////////////////////////////////RENDER FUNCTION////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function render() {
   renderPostCards();
@@ -190,7 +202,7 @@ function generatePostCard(index) {
                     <svg xmlns="http://www.w3.org/2000/svg" onclick="changeSvgH(${index})" width="24" height="24" fill="tomato" class="heart${index} bi bi-heart-fill d-none" viewBox="0 0 16 16">
                       <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" onclick="changeSvgC(${index})" width="25" height="25" fill="currentColor" class="chat${index} bi bi-chat" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" onclick="changeSvgC(${index}), openComments(${index})" width="25" height="25" fill="currentColor" class="chat${index} bi bi-chat" viewBox="0 0 16 16">
                         <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
                     </svg>
                     <svg xmlns="http://www.w3.org/2000/svg" onclick="changeSvgC(${index})" width="25" height="25" fill="black" class="chat${index} bi bi-chat-fill d-none" viewBox="0 0 16 16">
@@ -236,11 +248,6 @@ function generatePostCard(index) {
             </form>
         </div>
     </div>`;
-}
-
-function scrollToBottom(index) {
-  let element = document.getElementById(`commentOUT${index}`);
-  element.scrollTop += element.scrollHeight;
 }
 
 function generatePostComment(i, j) {
@@ -310,10 +317,27 @@ function changeSvgB(index) {
  * displays emojis on click
  */
 function emoji(i) {
-  let elem = document.getElementById(`emojiTable${i}`);
+  console.log('test')
+  if (i != undefined) {
+    let elem = document.getElementById(`emojiTable${i}`);
   elem.classList.toggle("d-none");
+  if (!emojiOpened) {
+    emojiOpened = true;
+  }
+  } else if (i == undefined && emojiOpened) {
+    for (let index = 0; index < posts.length; index++) {
+      document.getElementById(`emojiTable${index}`).classList.add('d-none');
+    }
+    emojiOpened = false
+  }
 }
 
+function openComments(index) {
+  let input = document.getElementById(`commentIN${index}`);
+
+  input.focus();
+  input.select();
+}
 ////////////////////////////////////////////////////////////////////////////////////////////SAVE & LOAD////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function saveApp() {
